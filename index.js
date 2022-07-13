2d1c84ebbf207324aafb38fca76e34268d412b27
horlarhyinka <danijufarouq2003@gmail.com>
2022-07-13 10:19:27 +0100

    #

--- index.js
+++ index.js
@@ -1,6 +1,26 @@
 const express = require("express");
 const dotenv = require("dotenv");
+const router = require("./routes.js");
 
+
+const app = express();
+
+//port variable
+const port = process.env.PORT || 5000;
 //configuring enviroment variable
 dotenv.config();
-const app = express();
+//setting up middlewares
+app.use(express.urlencoded({extended: true}));
+app.use(express.json());
+app.use(router);
+
+
+
+
+
+
+//listening to port
+app.listen(port,()=>{
+    console.log("now listening to port " + port)
+});
+
