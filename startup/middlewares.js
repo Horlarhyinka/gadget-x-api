fb409f3e8deb636ceecea8f2a242ce7509ebfcac
Horlarhyinka <danijufarouq2003@gmail.com>
2023-10-30 17:26:20 +0100

    update config/middleware

--- startup/middlewares.js
+++ startup/middlewares.js
@@ -10,7 +10,7 @@ app.use(express.json())
 app.use(express.urlencoded({extended:true}))
 app.use(require("cookie-parser")())
 app.use(cors({
-    origin: process.env.APP_API_URL,
+    origin: "*",
     credentials: true,
     optionSuccessStatus:200
     }))
