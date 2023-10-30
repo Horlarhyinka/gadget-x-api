d36411d013a20e2b8b65300966e6e04076d230fa
Horlarhyinka <danijufarouq2003@gmail.com>
2023-10-30 17:11:34 +0100

    update config/db.js

--- config/db.js
+++ config/db.js
@@ -3,8 +3,5 @@ const mongoose = require("mongoose");
 const log = require("../logger")
 
 module.exports.connectDB = (uri) =>{
-    if(process.env.NODE_ENV === "production"){
-       uri = uri.replace("<password>",process.env.DB_PASSWORD)
-    }
     return mongoose.connect(uri)
 }
