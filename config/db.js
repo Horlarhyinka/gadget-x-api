434643284ce34c3037b6401e7ed1bc0048109f6b
Horlarhyinka <danijufarouq2003@gmail.com>
2023-10-30 17:16:26 +0100

    update config/db.js

--- config/db.js
+++ config/db.js
@@ -2,6 +2,8 @@ require("dotenv").config()
 const mongoose = require("mongoose");
 const log = require("../logger")
 
+
 module.exports.connectDB = (uri) =>{
-    return mongoose.connect(uri)
+const options = {mongoUrl: uri}
+    return mongoose.connect(options.mongoUrl, options)
 }
