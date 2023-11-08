dac2f6fe3858a0eac616fc86a0d24fcd82ef0b0a
Horlarhyinka <danijufarouq2003@gmail.com>
2023-11-08 17:29:20 +0100

    update cookie option

--- controller/auth.js
+++ controller/auth.js
@@ -110,9 +110,9 @@ module.exports.resetPassword = async(req,res)=>{
 
 function sendCookie(payload,res){
     res.set("x-auth-token",payload)
-    let options = {expires:new Date(Date.now()+(2 * 24 * 3600 * 1000)), httpOnly: false, domain: process.env.APP_UI_URL }
+    let options = {expires:new Date(Date.now()+(2 * 24 * 3600 * 1000)), httpOnly: false, domain: process.env.NODE_ENV === "production"? ".vercel.app": "localhost" }
     if(process.env.NODE_ENV === "production"){
-        options = {...options, secure: true, sameSite: "Lax"}
+        options = {...options, secure: true, sameSite: "none"}
     }
     return res.cookie("x-auth-token",payload,options)
 }
